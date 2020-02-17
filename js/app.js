6b835ea01c4387607667e38d7d7f296716a2861f
ferin79 <ferinpatel79@gmail.com>
2020-02-17 12:21:53 +0530

    sw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('./sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then((reg) => console.log("Service Worker Register ", reg))
             .catch((err) => console.log("Failed To Register Service Worker ", err))
     });
