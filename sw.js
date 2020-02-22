dd87c78e0857a171fb9eba84360ac48eeff0a312
Ferin79 <ferinpatel79@gmail.com>
2020-02-22 20:17:00 +0530

    SW Fixed

--- sw.js
+++ sw.js
@@ -6,22 +6,24 @@ const files_to_cache = [
      './pages/departments.html',
      './css/index.css',
      './css/default.css',
+     './css/testimonial.css',
      './js/index.js',
      './js/darkmode.js',
+     './images/logo_dark.png',
+     './images/index_logo.png',
      'https://unpkg.com/aos@2.3.1/dist/aos.js',
      'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
      'https://code.jquery.com/jquery-3.4.1.min.js',
      'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
      'https://unpkg.com/aos@2.3.1/dist/aos.css',
      'https://fonts.googleapis.com/icon?family=Material+Icons',
-     './images/index_logo.png'
 ];
 
 self.addEventListener('install', (evt) => {
      evt.waitUntil(
           caches.open(cacheName)
                .then(cache => {
-                    return cache.addAll(files_to_cache).then(() => self.skipWaiting());
+                    return cache.addAll(files_to_cache).then();
                })
      )
 });
