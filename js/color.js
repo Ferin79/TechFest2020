b6739e450a2ea47d34b8592bc6b81771abaf9f18
Ferin79 <ferinpatel79@gmail.com>
2020-03-04 12:38:46 +0530

    Issue Fix

---
+++ js/color.js
@@ -0,0 +1,15 @@
+const color = [
+  "light-blue lighten-5",
+  "amber lighten-4",
+  "deep-orange lighten-3",
+  "amber lighten-3",
+  "cyan lighten-4",
+  "teal lighten-4"
+];
+
+$("#1").addClass(color[0]);
+$("#2").addClass(color[1]);
+$("#3").addClass(color[2]);
+$("#4").addClass(color[3]);
+$("#5").addClass(color[4]);
+$("#6").addClass(color[5]);
--- js/darkmode.js
+++ js/darkmode.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
-    $('.sidenav').sidenav();
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        dh = $(document).height();
-        wh = $(window).height();
-        var scrollpercent = (scroll / (dh - wh)) * 100;
-        $("#progress-bar").css('height', scrollpercent + '%');
-        $('.scroll_down').css('visibility', 'hidden');
-    });
-    let darkmode = parseInt(localStorage.getItem('darkmode'));
-    console.log(darkmode);
-    if (darkmode) {
-        if (Boolean(darkmode)) {
-            $('body').addClass('dark_mode');
-        } else {
-            $('body').removeClass('dark_mode');
-        }
+  $(".sidenav").sidenav();
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    dh = $(document).height();
+    wh = $(window).height();
+    var scrollpercent = (scroll / (dh - wh)) * 100;
+    $("#progress-bar").css("height", scrollpercent + "%");
+    $(".scroll_down").css("visibility", "hidden");
+  });
+  let darkmode = parseInt(localStorage.getItem("darkmode"));
+  console.log(darkmode);
+  if (darkmode) {
+    if (Boolean(darkmode)) {
+      $("body").addClass("dark_mode");
     } else {
-        localStorage.setItem('darkmode', '0');
+      $("body").removeClass("dark_mode");
     }
+  } else {
+    localStorage.setItem("darkmode", "0");
+  }
 
-    $(".toggle_dark_mode").click(() => {
-        if ($('body').hasClass('dark_mode')) {
-            $('body').removeClass('dark_mode');
-            localStorage.setItem('darkmode', '0');
-        } else {
-            $('body').addClass('dark_mode');
-            localStorage.setItem('darkmode', '1')
-        }
-    });
+  $(".toggle_dark_mode").click(function() {
+    if ($("body").hasClass("dark_mode")) {
+      $("body").removeClass("dark_mode");
+      localStorage.setItem("darkmode", "0");
+    } else {
+      $("body").addClass("dark_mode");
+      localStorage.setItem("darkmode", "1");
+    }
+  });
 });
